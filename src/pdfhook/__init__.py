a38484c45487ee6e27f3c5f7b6f25bf22e2c0e3b
brennv <brennv@users.noreply.github.com>
2016-05-31 08:27:12 -0700

    refactor init db

--- src/pdfhook/__init__.py
+++ src/pdfhook/__init__.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
-
-from flask import Blueprint
+from . import views
+from flask import Blueprint, current_app
+from sqlalchemy.engine.reflection import Inspector
 
 blueprint = Blueprint(
     'pdfhook', __name__,
 )
 
-from . import views
+@blueprint.before_app_first_request
+def make_sure_there_is_a_working_database(*args, **kwargs):
+    if current_app.config.get('ENV') != 'dev':
+        return
+    inspector = Inspector.from_engine(db.engine)
+    tables = inspector.get_table_names()
+    required_tables = [models.PDFForm.__tablename__]
+    if not (set(required_tables) < set(tables)):
+        current_app.logger.warning(
+            "database tables {} not found. Creating tables".format(required_tables))
+        db.create_all()
